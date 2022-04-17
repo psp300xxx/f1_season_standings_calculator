1154ce93d74b7367147671e0679fb18806f7ab1d
luigidemarco <ldm1294@gmail.com>
2022-04-17 14:40:27 +0200

    Added possibility to execute on multiple years at once, and to write it on multiple files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 import controller.*;
 import model.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
 public class Main {
+
+    public static final int MIN_ARGS_LEN = 1;
+
+    public static final int FIRST_F1_YEAR = 1950;
 
     private static Season season;
 
     public static class SeasonLoaderDelegateImpl implements SeasonLoaderDelegate {
+
+        private PrintWriter printWriter = new PrintWriter(System.out, true);
+        @Override
+        public PrintWriter getPrintWriter() {
+            return this.printWriter;
+        }
+
+        @Override
+        public void setPrintWriter(PrintWriter newPrintWriter) {
+            this.printWriter = newPrintWriter;
+        }
 
         @Override
         public void seasonLoaded(SeasonLoader loader, Season season) {
@@ -19,8 +39,9 @@ public class Main {
             PointSystem newPointSystem = pointSystemCalculator.newPointSystem(driversOnGrid, new PointSystem2010().getRatio());
             standings.setPointSystem(newPointSystem);
             standings.computeSeason(season);
-            System.out.println(newPointSystem.pointsPosition());
-            System.out.println(standings.standings(true));
+            getPrintWriter().write(newPointSystem.pointsPosition());
+            getPrintWriter().write(standings.standings(true));
+            getPrintWriter().flush();
         }
 
         @Override
@@ -29,9 +50,56 @@ public class Main {
         }
     }
 
+    public static class NotF1YearException extends Exception{
+        private int year;
+
+        public NotF1YearException(int year){
+            this.year = year;
+        }
+
+        @Override
+        public String getMessage() {
+            return String.format("%d is not ad F1 year", this.year);
+        }
+    }
+
+    private static int parseInput(String input) throws NumberFormatException, NotF1YearException{
+        int year = Integer.parseInt(input);
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        if(year<FIRST_F1_YEAR || year>currentYear){
+            throw new NotF1YearException(year);
+        }
+        return year;
+    }
+
+    private static void executeInput(String input, File output){
+        try{
+            int year = parseInput(input);
+            SeasonLoaderDelegateImpl newDelegate = new SeasonLoaderDelegateImpl();
+            if(output!=null){
+                PrintWriter filePrintWriter = new PrintWriter(output);
+                newDelegate.setPrintWriter(filePrintWriter);
+            }
+            SeasonLoaderThread seasonLoaderThread = new SeasonLoaderThread(new SeasonLoaderImpl(), newDelegate, year);
+            seasonLoaderThread.start();
+        }catch (NumberFormatException  exception){
+            System.err.println(String.format("'%s' is not a number", input));
+        }
+        catch (NotF1YearException exception){
+            System.err.println(exception.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        int year = Integer.parseInt(args[0]);
-        SeasonLoaderThread seasonLoaderThread = new SeasonLoaderThread(new SeasonLoaderImpl(), new SeasonLoaderDelegateImpl(), year);
-        seasonLoaderThread.start();
+        if(args.length<MIN_ARGS_LEN){
+            System.err.println("Usage: java -jar f1_season.jar [YEAR]");
+            System.exit(0);
+        }
+        for( int i =0  ; i<args.length; i++ ){
+            String currentOutputFile = String.format("./%s.txt", args[i]);
+            executeInput(args[i], new File(currentOutputFile));
+        }
     }
 }
--- src/main/java/controller/SeasonLoaderDelegate.java
+++ src/main/java/controller/SeasonLoaderDelegate.java
@@ -2,7 +2,13 @@ package controller;
 
 import model.Season;
 
+import java.io.PrintWriter;
+
 public interface SeasonLoaderDelegate {
+
+    PrintWriter getPrintWriter();
+
+    void setPrintWriter(PrintWriter newPrintWriter);
 
     void seasonLoaded(SeasonLoader loader, Season season);
 
